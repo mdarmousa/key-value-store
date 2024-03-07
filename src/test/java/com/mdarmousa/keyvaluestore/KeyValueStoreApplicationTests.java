e93aa2b9db6209fa8075a7ac1dbec3f7f3a8a5d6
Mohammad Darmousa <mdarmousa1@gmail.com>
2024-03-07 10:47:07 +0400

    KVS-0: Add files

---
+++ src/test/java/com/mdarmousa/keyvaluestore/KeyValueStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdarmousa.keyvaluestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyValueStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
